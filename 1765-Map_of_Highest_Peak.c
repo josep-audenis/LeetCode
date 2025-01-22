de71e132bd9cf9b447082ac9cbf2fb727c082033
pepinho02 <pepaudenisr@gmail.com>
2025-01-22 13:11:16 +0100

    22/01/2025 - 1765

---
+++ 1765-Map_of_Highest_Peak.c
@@ -0,0 +1,110 @@
+// You are given an integer matrix isWater of size m x n that represents a map of land and water cells.
+// · If isWater[i][j] == 0, cell (i, j) is a land cell.
+// · If isWater[i][j] == 1, cell (i, j) is a water cell.
+// 
+// You must assign each cell a height in a way that follows these rules:
+// · The height of each cell must be non-negative.
+// · If the cell is a water cell, its height must be 0.
+// · Any two adjacent cells must have an absolute height difference of at most 1. A cell is adjacent to another cell if the former is directly north, east, south, or west of the latter (i.e., their sides are touching).
+// 
+// Find an assignment of heights such that the maximum height in the matrix is maximized.
+// Return an integer matrix height of size m x n where height[i][j] is cell (i, j)'s height. If there are multiple solutions, return any of them.
+// 
+// - Example 1:
+// Input: isWater = [[0,1],[0,0]]
+// Output: [[1,0],[2,1]]
+// Explanation: The image shows the assigned heights of each cell.
+// The blue cell is the water cell, and the green cells are the land cells.
+// 
+// - Example 2:
+// Input: isWater = [[0,0,1],[1,0,0],[0,0,0]]
+// Output: [[1,1,0],[0,1,1],[1,2,2]]
+// Explanation: A height of 2 is the maximum possible height of any assignment.
+// Any height assignment that has a maximum height of 2 while still meeting the rules will also be accepted.
+//  
+// Constraints:
+// · m == isWater.length
+// · n == isWater[i].length
+// · 1 <= m, n <= 1000
+// · isWater[i][j] is 0 or 1.
+// · There is at least one water cell.
+
+const int dirs[8][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+int isOutside(int i, int j, int maxR, int maxC) {
+    return (i < 0 || j < 0 || i >= maxR || j >= maxC);
+}
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** highestPeak(int** isWater, int isWaterSize, int* isWaterColSize, int* returnSize, int** returnColumnSizes) {
+    int r = isWaterSize;
+    int c = *isWaterColSize;
+
+    *returnSize = isWaterSize;
+    *returnColumnSizes = (int *)malloc(sizeof(int) * *returnSize);
+
+    int ** map = malloc(r * sizeof(int *));
+    for (int i = 0; i < r; i++) {
+        
+        (*returnColumnSizes)[i] = *isWaterColSize;
+        map[i] = malloc(c * sizeof(int));
+        if (c > 1) {
+            memset(map[i], 10000, sizeof(int) * c);
+        } else {
+            map[i][0] = 10000;
+        }
+    } 
+
+    int minH, ai, aj, value;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if (isWater[i][j]) {
+                map[i][j] = 0;
+                continue;
+            }
+            minH = (r*c) + 1;
+            for (int d = 0; d < 4; d++) {
+                ai = i + dirs[d][0];
+                aj = j + dirs[d][1];
+
+                if (isOutside(ai, aj, r, c)) continue;
+                
+                value = (isWater[ai][aj] ? 0 : map[ai][aj]);
+
+                minH = (minH < value ? minH : value);
+            }
+
+            map[i][j] = minH+1;
+        }
+    }
+
+    for (int i = r - 1; i >= 0; i--) {
+        for (int j = c - 1; j >= 0; j--) {
+            if (isWater[i][j]) continue;
+            minH = (r*c) + 1;
+            for (int d = 0; d < 4; d++) {
+                ai = i + dirs[d][0];
+                aj = j + dirs[d][1];
+
+                if (isOutside(ai, aj, r, c)) continue;
+                
+                value = map[ai][aj];
+
+                minH = (minH < value ? minH : value);
+            }
+
+            map[i][j] = minH+1;
+        }
+    }
+
+    return map;
+}
+
+// Runtime: 55ms 
+// Memory: 171.08MB
+
+//This is a very suboptimal solution, it would have been better to do a BFS with all the water squares first, but I wasn't in the mood...
