a032074084094a2897ddd72950b7f18b9af1c90b
pepinho02 <pepaudenisr@gmail.com>
2025-01-26 23:46:14 +0100

    last comments I promise

--- 0802-Find_Eventual_Safe_States.c
+++ 0802-Find_Eventual_Safe_States.c
@@ -26,17 +26,21 @@
 // · The number of edges in the graph will be in the range [1, 4 * 104].
 
 bool dfs(int index, bool * visit, bool * stack, int ** graph, int n, int * nSafe, int * nCol) {
-    if (stack[index]) return true;
-    if (visit[index]) return false;
+    if (stack[index]) return true;                                                                      //if the current node is in the recursion stack, a cycle is detected
+    if (visit[index]) return false;                                                                     //if the node has already been visited and is not in a cycle
+    
     *nSafe -= 1;                                                                                        //first assume that the node is not safe
-    stack[index] = true;
-    visit[index] = true;
-    for (int i = 0; i < nCol[index]; i++) {                                                             
-        if (dfs(graph[index][i], visit, stack, graph, n, nSafe, nCol)) return true;
+    stack[index] = true;                                                                                //mark the current node as part of the recursion stack
+    visit[index] = true;                                                                                //mark the current node as visited
+    
+    for (int i = 0; i < nCol[index]; i++) {                                                             //recursively check all neighbors of the current node    
+        if (dfs(graph[index][i], visit, stack, graph, n, nSafe, nCol)) return true;                     //and return if any neighbor leads to a cycle
     }
-    *nSafe += 1;
-    stack[index] = false;
-    return false;
+    
+    *nSafe += 1;                                                                                        //if no cycle is detected, mark the node as safe
+    stack[index] = false;                                                                               //remove the current node from the recursion stack
+    
+    return false;                                                                                       //no cycle detected
 }
 
 /**
@@ -46,20 +50,20 @@ int* eventualSafeNodes(int** graph, int graphSize, int* graphColSize, int* retur
     int n = graphSize;
     *returnSize = graphSize;
     bool * visit = malloc(sizeof(bool) * n);
-    bool * stack = malloc(sizeof(bool) * n);
+    bool * stack = malloc(sizeof(bool) * n);                                                            //array to track nodes in the current recursion stack
     memset(visit, 0, sizeof(bool) * n);
     memset(stack, 0, sizeof(bool) * n);
 
     for (int i = 0; i < n; i++) {
-        dfs(i, visit, stack, graph, n, returnSize, graphColSize);
+        dfs(i, visit, stack, graph, n, returnSize, graphColSize);                                       //perform dfs for each node to determine if it is part of a cycle
     }
 
-    int * safe = malloc(sizeof(int) * *returnSize);
+    int * safe = malloc(sizeof(int) * *returnSize);                                                     
     int index = 0;
     for (int i = 0; i < n; i++) {
-    if (!stack[i]) {                                                                                    //check if the node is not in the stack
+    if (!stack[i]) {                                                                                    //check if the node is not in the recursion stack
         safe[index] = i;                                                                                //construct the safe nodes array that will be returned
-            index++;
+            index++;                                                                                    //add the safe node to the result array
         }
     }
     return safe;
