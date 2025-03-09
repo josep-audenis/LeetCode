c52b73036a6c3b843a91f8ba673c5a03f9bb8ded
Josep Audenis <pepaudenisr@gmail.com>
2025-03-09 22:41:02 +0100

    3208

---
+++ 3208-Alternating_Groups_II.py
@@ -0,0 +1,60 @@
+# There is a circle of red and blue tiles. You are given an array of integers colors and an integer k. The color of tile i is represented by colors[i]:
+# · colors[i] == 0 means that tile i is red.
+# · colors[i] == 1 means that tile i is blue.
+
+# An alternating group is every k contiguous tiles in the circle with alternating colors (each tile in the group except the first and last one has a different color from its left and right tiles).
+
+# Return the number of alternating groups.
+
+# Note that since colors represents a circle, the first and the last tiles are considered to be next to each other.
+
+# - Example 1:
+# Input: colors = [0,1,0,1,0], k = 3
+# Output: 3
+
+# - Example 2:
+# Input: colors = [0,1,0,0,1,0,1], k = 6
+# Output: 2
+
+# - Example 3:
+# Input: colors = [1,1,0,1], k = 4
+# Output: 0
+
+# Constraints:
+# ·3 <= colors.length <= 105
+# ·0 <= colors[i] <= 1
+# ·3 <= k <= colors.length
+
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        length = len(colors)
+        result = 0
+        count = 1
+        last = colors[0]
+
+        for i in range(1, length):
+            if colors[i] == last:
+                count = 1
+                last = colors[i]
+                continue
+
+            count += 1
+            if count >= k:
+                result += 1
+
+            last = colors[i]
+
+        for i in range(k - 1):
+            if colors[i] == last:
+                break
+
+            count += 1
+            if count >= k:
+                result += 1
+
+            last = colors[i]
+
+        return result
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
